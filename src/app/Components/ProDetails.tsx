5b88b80f75703bd606fea76bfcfd3fcdc498cdfe
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2025-01-12 17:36:12 +0500

    second side has been started

--- src/app/Components/ProDetails.tsx
+++ src/app/Components/ProDetails.tsx
@@ -3,15 +3,43 @@ import React from 'react'
 const ProDetails = () => {
     return (
         <div>
-            <div className='h-[286] w-[1920] bg-[#F6F5FF] py-[140px] px-[200px]'>
-                <h1 className='h-[36px] w-[256px] font-JosefinSans text-[29px] text-[#101750] font-semibold text-3xl'> Products Details</h1>
+            {/* first side started */}
+            <div className='bg-[#F6F5FF] py-10 px-6 md:py-[140px] md:px-[200px]'>
+                <h1 className='font-JosefinSans text-[24px] text-[#101750] font-semibold md:text-[29px] text-3xl'>
+                    Products Details
+                </h1>
 
-                <div className='flex gap-2 py-[9px]'>
-                    <div><p className='text-[16px] font-light text-[#000000]'>Home ,</p></div>
-                    <div><p className='text-[16px] font-light text-[#000000]'>Page ,</p></div>
-                    <div><p className='text-[#FB2E86]'>Products Details</p></div>
+                <div className='flex flex-wrap gap-2 py-4'>
+                    <div>
+                        <p className='text-[14px] md:text-[16px] font-light text-[#000000]'>Home ,</p>
+                    </div>
+                    <div>
+                        <p className='text-[14px] md:text-[16px] font-light text-[#000000]'>Page ,</p>
+                    </div>
+                    <div>
+                        <p className='text-[14px] md:text-[16px] text-[#FB2E86]'>Products Details</p>
+                    </div>
                 </div>
             </div>
+            {/* first side completed  */}
+
+            {/* second side started */}
+            <div className='bg-slate-600 py-[80px] px-[60px]'>
+                <div className='w-[1170px] h-[509px] bg-blue-400'>
+
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
 
         </div>
     )
