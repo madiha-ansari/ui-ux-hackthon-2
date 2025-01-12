f3b081f302ba0c6b19bf10392e4e959dcdf47300
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2025-01-12 17:11:13 +0500

    div set the products details

--- src/app/Components/ProDetails.tsx
+++ src/app/Components/ProDetails.tsx
@@ -4,11 +4,12 @@ const ProDetails = () => {
     return (
         <div>
             <div className='h-[286] w-[1920] bg-[#F6F5FF] py-[140px] px-[200px]'>
-                <h1 className=''> Products Details</h1>
+                <h1 className='h-[36px] w-[256px] font-JosefinSans text-[29px] text-[#101750] font-semibold text-3xl'> Products Details</h1>
+               
                 <div className='flex gap-2'>
-                    <div>Home ,</div>
-                    <div>Page ,</div>
-                    <div>Products</div>
+                    <div><p>Home ,</p></div>
+                    <div><p>Page ,</p></div>
+                    <div><p>Products</p></div>
                 </div>
             </div>
 
