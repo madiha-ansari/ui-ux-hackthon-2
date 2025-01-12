83b499648558954db012e54ea93de480f1938fd9
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2025-01-12 17:19:19 +0500

    setting completed

--- src/app/Components/ProDetails.tsx
+++ src/app/Components/ProDetails.tsx
@@ -5,11 +5,11 @@ const ProDetails = () => {
         <div>
             <div className='h-[286] w-[1920] bg-[#F6F5FF] py-[140px] px-[200px]'>
                 <h1 className='h-[36px] w-[256px] font-JosefinSans text-[29px] text-[#101750] font-semibold text-3xl'> Products Details</h1>
-               
-                <div className='flex gap-2'>
-                    <div><p>Home ,</p></div>
-                    <div><p>Page ,</p></div>
-                    <div><p>Products</p></div>
+
+                <div className='flex gap-2 py-[9px]'>
+                    <div><p className='text-[16px] font-light text-[#000000]'>Home ,</p></div>
+                    <div><p className='text-[16px] font-light text-[#000000]'>Page ,</p></div>
+                    <div><p className='text-[#FB2E86]'>Products Details</p></div>
                 </div>
             </div>
 
