738df286248ec8331adb585dd344acf697003322
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2025-01-12 16:58:55 +0500

    product details page

--- src/app/Components/ProDetails.tsx
+++ src/app/Components/ProDetails.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
 const ProDetails = () => {
-  return (
-    <div>
-        <div className='h-[286] w-[1920] bg-[#F6F5FF] p-[125]'>
-Products Details
+    return (
+        <div>
+            <div className='h-[286] w-[1920] bg-[#F6F5FF] py-[140px] px-[200px]'>
+               <h1> Products Details</h1>
+               <div>
+                <div>Home</div>
+                <div>Page</div>
+                <div></div>
+               </div>
+            </div>
+
         </div>
-      
-    </div>
-  )
+    )
 }
 
 export default ProDetails
