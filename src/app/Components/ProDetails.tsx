e2fbc13114abdf2f68aef31c7af17467fb3e09b4
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2025-01-12 17:00:34 +0500

    heading sets

--- src/app/Components/ProDetails.tsx
+++ src/app/Components/ProDetails.tsx
@@ -4,12 +4,12 @@ const ProDetails = () => {
     return (
         <div>
             <div className='h-[286] w-[1920] bg-[#F6F5FF] py-[140px] px-[200px]'>
-               <h1> Products Details</h1>
-               <div>
-                <div>Home</div>
-                <div>Page</div>
-                <div></div>
-               </div>
+                <h1 className=''> Products Details</h1>
+                <div className='flex gap-2'>
+                    <div>Home ,</div>
+                    <div>Page ,</div>
+                    <div>Products</div>
+                </div>
             </div>
 
         </div>
